258575e515d70c4119df5c65f1d26210b4d65b33
Jesús García Ortuño <jesuscnnbs@gmail.com>
2025-02-20 17:49:33 +0100

    add metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
 import { Inter } from "next/font/google";
 import { HomPage } from "@/components";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={inter.className}>
-      <HomPage />
-    </main>
+    <>
+      <Head>
+        <title>Portfolio - jesuscnnbs</title>
+        <meta name="description" content="Portfolio web page for jesuscnnbs front-end developer" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="/" />
+        <meta property="og:title" content="Portfolio - jesuscnnbs" />
+        <meta property="og:description" content="Portfolio web page for jesuscnnbs front-end developer" />
+        <meta property="og:image" content="/frontend-bg.jpg" />
+        
+
+        <link rel="canonical" href="/" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={inter.className}>
+        <HomPage />
+      </main>
+    </>
   );
 }
