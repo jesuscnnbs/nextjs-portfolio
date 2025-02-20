fdf66bdf459deb7e2b153bde5d1b091d8753a351
Jesús García Ortuño <jesuscnnbs@gmail.com>
2025-02-20 22:20:20 +0100

    fix visual issues

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -11,7 +11,7 @@ export const HomPage = () => {
   return (
     <div className="grid grid-cols-[54px_1fr]">
       <SideBar />
-      <main className="bg-main-element">
+      <main className="bg-main-element bg-cover bg-center">
         <Header />
         <div className="mx-auto max-w-5xl px-4 md:px-8 space-y-32 pb-24 overflow-hidden">
           <Hero />
